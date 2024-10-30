1cdd582df6526637a63d8b3d3f75f8b6c1dce1c1
manvithagali52 <manvithag52@gmail.com>
2024-10-30 13:28:42 +0530

    updated code

--- SQL/scripts.sql
+++ SQL/scripts.sql
@@ -1,8 +1,8 @@
-create Schema 2024_batch;
-use 2024_batch;
+create Schema 2024_batch_mobile_shop;
+use 2024_batch_mobile_shop;
 create table stock(id int auto_increment primary key, type varchar(200), qty  int);
 create table mobile(product_brand varchar(50),product_price double,product_model varchar(50),arrived_date_time datetime);
-create table charger(brand varchar(50),price double,model varchar(50),arrived_date_time datetime);
-create table powerbank(brand varchar(50),price double,model varchar(50),arrived_date_time datetime);
-create table headset(brand varchar(50),price double,model varchar(50),arrived_date_time datetime);
-create table backcover(brand varchar(50),price double,model varchar(50),arrived_date_time datetime);
+create table charger(product_brand varchar(50),product_price double,product_model varchar(50),arrived_date_time datetime);
+create table powerbank(product_brand varchar(50),product_price double,product_model varchar(50),arrived_date_time datetime);
+create table backcover(product_brand varchar(50),product_price double,product_model varchar(50),arrived_date_time datetime);
+create table headset(product_brand varchar(50),product_price double,product_model varchar(50),arrived_date_time datetime);
